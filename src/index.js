cea9b41e748f0b11ca7e6d87e6adffeec5289de5
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-06-28 22:12:05 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import * as basicLightbox from 'basiclightbox';
+import "../node_modules/basiclightbox/dist/basicLightbox.min.css";
+
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+// `)
+
+// instance.show();
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
