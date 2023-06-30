4a1a5b4b57a3c976b7c371d1ea780927fda89133
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-06-30 11:21:55 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import * as basicLightbox from 'basiclightbox';
 import "../node_modules/basiclightbox/dist/basicLightbox.min.css";
+import { submitHundler } from "./js/templateStringHandler.js";
 
-const instance = basicLightbox.create(`
-    <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
-`)
+// const instance = basicLightbox.create(`
+//     <iframe src="https://www.youtube.com/embed/E1oZhEIrer4" width="560" height="315" frameborder="0"></iframe>
+// `)
 
-instance.show()
+// instance.show()
+
+const refs = {
+    form: document.querySelector('#form'),
+    input: document.querySelector('#add'),
+    jsMarkup: document.querySelector('.jsmarkup')
+}
+
+refs.form.addEventListener("submit", submitHundler);
--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,12 +1,24 @@
 import refs from "./refs.js";
-import serverData from "../data/serverData.js";
-import questionsTemplate from "../templates/questions.hbs";
+// import serverData from "../data/serverData.js";
+// import questionsTemplate from "../templates/questions.hbs";
 
 export function submitHundler(e) {
     e.preventDefault();
     const value = refs.input.value;
-    serverData.push(value);
+    // serverData.push(value);
+    // refs.input.value = "";
+    // const markup = questionsTemplate(serverData);
+    // refs.root.innerHTML = markup;
+    fetch("http://localhost:7777/items", {
+        method: "POST",
+        body: JSON.stringify({ text: value }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error))
+    // refs.ul.insertAdjacentHTML("beforeend", creatLi(value));
     refs.input.value = "";
-    const markup = questionsTemplate(serverData);
-    refs.root.innerHTML = markup;
 }
